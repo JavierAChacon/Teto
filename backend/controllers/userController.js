57c314a3c6cb6752b4aaf44aa103775d2ac985d5
Alejandro Chacón <alejandro.chr@outlook.es>
2023-08-09 02:13:36 -0400

    added the remaining user-related endpoints

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -17,7 +17,7 @@ const register = async (req, res) => {
   } catch (error) {
     console.log(error);
   }
-};
+}
 
 const authenticate = async (req, res) => {
   const { email, password } = req.body;
@@ -41,16 +41,16 @@ const authenticate = async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
-      token: generateJWT(user._id)
+      token: generateJWT(user._id),
     });
   }
-};
+}
 
 const confirm = async (req, res) => {
   const { token } = req.params;
   const userConfirm = await User.findOne({ token });
 
-  if(!userConfirm){
+  if (!userConfirm) {
     const error = new Error("Token no valid");
     return res.status(403).json({ msg: error.message });
   }
@@ -65,4 +65,65 @@ const confirm = async (req, res) => {
   }
 }
 
-export { register, authenticate, confirm };
+const forgotPassword = async (req, res) => {
+  const { email } = req.body;
+  //verificar si existe el usuario
+  const user = await User.findOne({ email });
+  if (!user) {
+    const error = new Error("The user does not exist");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  try {
+    user.token = generateId();
+    await user.save();
+    res.json({ msg: "We have sent you an email with the instructions" });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const validateToken = async (req, res) => {
+  const { token } = req.params;
+  //validar que existe token
+  const validToken = await User.findOne({ token });
+
+  if (validToken) {
+    res.json({ msg: "Valid token" });
+  } else {
+    const error = new Error("No valid token");
+    return res.status(404).json({ msg: error.message });
+  }
+}
+
+const setNewPassword = async (req, res) => {
+  const { token } = req.params;
+  const { password } = req.body;
+
+  const user = await User.findOne({ token });
+
+  if (user){
+    user.password = password;
+    user.token = "";
+    try {
+      await user.save();
+      res.json({ msg: "Changed password succesfully" })
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+const profile = async (req, res) => {
+  res.json({ msg: "Ya estas en profile yupi!" });
+}
+
+export {
+  register,
+  authenticate,
+  confirm,
+  forgotPassword,
+  validateToken,
+  setNewPassword,
+  profile
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,25 @@
 import express from "express";
-import { register, authenticate, confirm } from "../controllers/userController.js";
+import {
+  register,
+  authenticate,
+  confirm,
+  forgotPassword,
+  validateToken,
+  setNewPassword,
+  profile
+} from "../controllers/userController.js";
+
+import checkAuth from "../middlewares/checkAuth.js";
 
 const router = express.Router();
 
 router.post("/", register); //create a new user
-
 router.post("/login", authenticate); //authenticate a user
+router.get("/confirm/:token", confirm); //confirm a user
+router.post("/forgot-password", forgotPassword); //validate user and send email with the new token
 
-router.get("/confirm/:token", confirm)
+router.route("/forgot-password/:token").get(validateToken).post(setNewPassword);
+
+router.get("/profile", checkAuth, profile);
 
 export default router;
