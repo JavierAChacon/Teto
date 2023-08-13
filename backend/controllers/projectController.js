026679d0c3a3107f36ed47573cc8cb18ef38926c
Alejandro Chacón <alejandro.chr@outlook.es>
2023-08-13 16:24:54 -0400

    added the rest of controllers to the section of project

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -1,33 +1,96 @@
 import Project from "../models/Project.js";
 
 const getProjects = async (req, res) => {
-}
+  const projects = await Project.find().where("creator").equals(req.user);
+
+  res.json(projects);
+};
 
 const newProject = async (req, res) => {
-}
+  const project = new Project(req.body);
+  project.creator = req.user._id;
+
+  try {
+    const storedProject = await project.save();
+    res.json(storedProject);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const getProject = async (req, res) => {
-}
+  const { id } = req.params;
+  const project = await Project.findById(id);
+
+  if (!project) {
+    const error = new Error("Project not found");
+    return res.status(404).json({ msg: error.message });
+  } else if (project.creator.toString() !== req.user._id.toString()) {
+    const error = new Error("No valid action");
+    return res.status(401).json({ msg: error.message });
+  }
+
+  res.json(project);
+};
 
 const editProject = async (req, res) => {
-}
+  const { id } = req.params;
+  const project = await Project.findById(id);
+
+  if (!project) {
+    const error = new Error("Project not found");
+    return res.status(404).json({ msg: error.message });
+  } else if (project.creator.toString() !== req.user._id.toString()) {
+    const error = new Error("No valid action");
+    return res.status(401).json({ msg: error.message });
+  }
+
+  project.name = req.body.name || project.name;
+  project.description = req.body.description || project.description;
+  project.deliveryDate = req.body.deliveryDate || project.deliveryDate;
+  project.client = req.body.client || project.client;
+
+  try {
+    const storagedProject = await project.save();
+    res.json(storagedProject);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const deleteProject = async (req, res) => {
-}
+  const { id } = req.params;
+  const project = await Project.findById(id);
 
-const addCollaborator = async (req, res) => { }
+  if (!project) {
+    const error = new Error("Project not found");
+    return res.status(404).json({ msg: error.message });
+  } else if (project.creator.toString() !== req.user._id.toString()) {
+    const error = new Error("No valid action");
+    return res.status(401).json({ msg: error.message });
+  }
 
-const deleteCollaborator = async (req, res) => { }
+  try{
+    await project.deleteOne();
+    res.json({ msg: "Project deleted"});
+  }catch(error){
+    console.log(error);
+  }
+};
 
-const getTasks = async (req, res) => { }
+const addCollaborator = async (req, res) => {};
+
+const deleteCollaborator = async (req, res) => {};
+
+const getTasks = async (req, res) => {};
 
 export {
-    getProjects,
-    newProject,
-    getProject,
-    editProject,
-    deleteProject,
-    addCollaborator,
-    deleteCollaborator,
-    getTasks
-}
+  getProjects,
+  newProject,
+  getProject,
+  editProject,
+  deleteProject,
+  addCollaborator,
+  deleteCollaborator,
+  getTasks,
+};
