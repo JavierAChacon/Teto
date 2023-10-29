f9a61a6f5beb766d8e955a7f9386ca4d659c4fb1
Alejandro Chacón <alejandro.chr@outlook.es>
2023-10-29 16:03:49 -0400

    changed  for '' in page of forgotpassword

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -18,7 +18,7 @@ const ForgotPassword = () => {
     }
 
     try {
-      const { data } = await axiosClient.post(`/users/forgot-password`, {
+      const { data } = await axiosClient.post('/users/forgot-password', {
         email
       })
       setAlert({
@@ -39,7 +39,7 @@ const ForgotPassword = () => {
     <div className='min-h-screen bg-blue-100 p-[5%] md:p-0'>
       <div className='registerImage flex h-60 flex-col justify-center overflow-hidden rounded-t-lg px-[5%] pt-6 text-center text-white md:h-[30rem] md:rounded-none xl:h-[22rem]'>
         <h1 className='text-5xl font-bold  md:text-6xl'>Recover your access</h1>
-        <p className='mt-3 md:text-xl'>Don't loose your projects</p>
+        <p className='mt-3 md:text-xl'>Do not loose your projects</p>
       </div>
       <form
         className='md:text-normal mx-auto flex flex-col gap-y-2 rounded-b-lg bg-white p-3 md:relative md:bottom-20 md:w-[30rem] md:gap-y-3 md:rounded-lg md:text-lg'
